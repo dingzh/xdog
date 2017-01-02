dc548be5b4f75d410dfde31485c6c8eff0fdc518
Dean <zhouding@sjtu.edu.cn>
2017-01-02 13:41:03 +0800

    fix compatibility to CUDA7.5

--- gpu/gpu_stgauss2.cu
+++ gpu/gpu_stgauss2.cu
@@ -15,11 +15,13 @@
 //
 #include "gpu_stgauss2.h"
 #include "gpu_st.h"
-#include "gpu_sampler.h"
+#include "gpu_image.h"
+// #include "gpu_sampler.h"
 
 
 static texture<float, 2, cudaReadModeElementType> s_texSRC1;
 static texture<float4, 2, cudaReadModeElementType> s_texSRC4;
+<<<<<<< HEAD
 
 inline __host__ __device__ texture<float,2>& texSRC1() { return s_texSRC1; }
 inline __host__ __device__ texture<float4,2>& texSRC4() { return s_texSRC4; }
@@ -27,6 +29,82 @@ inline __host__ __device__ texture<float4,2>& texSRC4() { return s_texSRC4; }
 static texture<float4, 2, cudaReadModeElementType> s_texST;
 inline __host__ __device__ texture<float4,2>& texST() { return s_texST; }
 
+=======
+static texture<float4, 2, cudaReadModeElementType> s_texST;
+
+// inline __host__ __device__ texture<float , 2>& texSRC1() { return s_texSRC1; }
+// inline __host__ __device__ texture<float4, 2>& texSRC4() { return s_texSRC4; }
+// inline __host__ __device__ texture<float4, 2>& texST() { return s_texST; }
+
+
+struct gpu_sampler_SRC1 {
+    __host__ gpu_sampler_SRC1(const gpu_image<float>& img, cudaTextureFilterMode filter_mode=cudaFilterModePoint) {
+        s_texSRC1.filterMode = filter_mode;
+        GPU_SAFE_CALL(cudaBindTexture2D(0, s_texSRC1, img.ptr(), img.w(), img.h(), img.pitch()));
+    }
+
+    __host__ ~gpu_sampler_SRC1() {
+        s_texSRC1.filterMode = cudaFilterModePoint;
+        cudaUnbindTexture(s_texSRC1);
+    }
+    
+    __device__ float operator()(float x, float y) const { 
+        return tex2D(s_texSRC1, x, y); 
+    } 
+};
+
+
+struct gpu_sampler_SRC4 {
+    __host__ gpu_sampler_SRC4(const gpu_image<float4>& img, cudaTextureFilterMode filter_mode=cudaFilterModePoint) {
+        s_texSRC4.filterMode = filter_mode;
+        GPU_SAFE_CALL(cudaBindTexture2D(0, s_texSRC4, img.ptr(), img.w(), img.h(), img.pitch()));
+    }
+
+    __host__ ~gpu_sampler_SRC4() {
+        s_texSRC4.filterMode = cudaFilterModePoint;
+        cudaUnbindTexture(s_texSRC4);
+    }
+    
+    __device__ float4 operator()(float x, float y) const { 
+        return tex2D(s_texSRC4, x, y); 
+    } 
+};
+
+struct gpu_sampler_ST {
+    __host__ gpu_sampler_ST(const gpu_image<float4>& img, cudaTextureFilterMode filter_mode=cudaFilterModePoint) {
+        s_texST.filterMode = filter_mode;
+        GPU_SAFE_CALL(cudaBindTexture2D(0, s_texST, img.ptr(), img.w(), img.h(), img.pitch()));
+    }
+
+    __host__ ~gpu_sampler_ST() {
+        s_texST.filterMode = cudaFilterModePoint;
+        cudaUnbindTexture(s_texST);
+    }
+    
+    __device__ float4 operator()(float x, float y) const { 
+        return tex2D(s_texST, x, y); 
+    } 
+};
+
+struct gpu_resampler {
+    float2 s_;
+
+    __host__ gpu_resampler(const gpu_image<float4>& img, float2 s, cudaTextureFilterMode filter_mode=cudaFilterModePoint) {
+        s_ = s;
+        s_texST.filterMode = filter_mode;
+        GPU_SAFE_CALL(cudaBindTexture2D(0, s_texST, img.ptr(), img.w(), img.h(), img.pitch()));
+    }
+
+    __host__ ~gpu_resampler() {
+        s_texST.filterMode = cudaFilterModePoint;
+        cudaUnbindTexture(s_texST);
+    }
+
+    __device__ float4 operator()(float x, float y) const { 
+        return tex2D(s_texST, s_.x * x, s_.x * y); 
+    } 
+};
+>>>>>>>   fix compatibility to CUDA7.5
 
 template <typename T, typename SRC>
 struct stgauss2_filter {
@@ -87,6 +165,7 @@ gpu_image<float> gpu_stgauss2_filter( const gpu_image<float>& src, const gpu_ima
     if (sigma <= 0) return src;
     gpu_image<float> dst(src.size());
 
+<<<<<<< HEAD
     gpu_sampler<float, texSRC1> src_sampler(src, src_linear? cudaFilterModeLinear : cudaFilterModePoint);
     float cos_max = cosf(radians(max_angle));
 
@@ -98,6 +177,22 @@ gpu_image<float> gpu_stgauss2_filter( const gpu_image<float>& src, const gpu_ima
     } else {
         float2 s = make_float2((float)st.w() / src.w(), (float)st.h() / src.h());
         gpu_resampler<float4, texST> st_sampler(st, s, st_linear? cudaFilterModeLinear : cudaFilterModePoint);
+=======
+    gpu_sampler_SRC1 src_sampler(src, src_linear? cudaFilterModeLinear : cudaFilterModePoint);
+    float cos_max = cosf(radians(max_angle));
+
+    if (src.size() == st.size()) {
+        gpu_sampler_ST st_sampler(st, st_linear? cudaFilterModeLinear : cudaFilterModePoint);
+        if (order == 1) 
+            imp_stgauss2_filter<1,float><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
+        else if (order == 2) 
+            imp_stgauss2_filter<2,float><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
+        else if (order == 4) 
+            imp_stgauss2_filter<4,float><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
+    } else {
+        float2 s = make_float2((float)st.w() / src.w(), (float)st.h() / src.h());
+        gpu_resampler st_sampler(st, s, st_linear? cudaFilterModeLinear : cudaFilterModePoint);
+>>>>>>>   fix compatibility to CUDA7.5
         if (order == 1) imp_stgauss2_filter<1,float><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
         else if (order == 2) imp_stgauss2_filter<2,float><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
         else if (order == 4) imp_stgauss2_filter<4,float><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
@@ -115,17 +210,29 @@ gpu_image<float4> gpu_stgauss2_filter( const gpu_image<float4>& src, const gpu_i
     if (sigma <= 0) return src;
     gpu_image<float4> dst(src.size());
 
+<<<<<<< HEAD
     gpu_sampler<float4, texSRC4> src_sampler(src, src_linear? cudaFilterModeLinear : cudaFilterModePoint);
     float cos_max = cosf(radians(max_angle));
 
     if (src.size() == st.size()) {
         gpu_sampler<float4, texST> st_sampler(st, st_linear? cudaFilterModeLinear : cudaFilterModePoint);
+=======
+    gpu_sampler_SRC4 src_sampler(src, src_linear? cudaFilterModeLinear : cudaFilterModePoint);
+    float cos_max = cosf(radians(max_angle));
+
+    if (src.size() == st.size()) {
+        gpu_sampler_ST st_sampler(st, st_linear? cudaFilterModeLinear : cudaFilterModePoint);
+>>>>>>>   fix compatibility to CUDA7.5
         if (order == 1) imp_stgauss2_filter<1,float4><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
         else if (order == 2) imp_stgauss2_filter<2,float4><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
         else if (order == 4) imp_stgauss2_filter<4,float4><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
     } else {
         float2 s = make_float2((float)st.w() / src.w(), (float)st.h() / src.h());
+<<<<<<< HEAD
         gpu_resampler<float4, texST> st_sampler(st, s, st_linear? cudaFilterModeLinear : cudaFilterModePoint);
+=======
+        gpu_resampler st_sampler(st, s, st_linear? cudaFilterModeLinear : cudaFilterModePoint);
+>>>>>>>   fix compatibility to CUDA7.5
         if (order == 1) imp_stgauss2_filter<1,float4><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
         else if (order == 2) imp_stgauss2_filter<2,float4><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
         else if (order == 4) imp_stgauss2_filter<4,float4><<<dst.blocks(), dst.threads()>>>(dst, src_sampler, st_sampler, sigma, cos_max, adaptive, step_size, precision);
